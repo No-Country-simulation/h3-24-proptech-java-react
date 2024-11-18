6e2f620b733134caed2129b0f7517ae71cbf9104
Catriel Escobar <125727012+Catriel-Escobar@users.noreply.github.com>
2024-11-18 04:34:59 -0300

    fix mapper

--- backend/src/main/java/com/financial/config/mapper/UserMapper.java
+++ backend/src/main/java/com/financial/config/mapper/UserMapper.java
@@ -11,7 +11,14 @@ import org.mapstruct.factory.Mappers;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-//    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    @Mapping(source = "userId",target = "userId")
+    @Mapping(source = "email",target = "email")
+    @Mapping(source = "name",target = "name")
+    @Mapping(source = "lastname",target = "lastname")
+    @Mapping(source = "dni",target = "dni")
+    @Mapping(source = "roles",target = "roles")
     UserResponseDto toUserResponseDTO(User user);
 
 }
