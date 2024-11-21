36d985f123cca7fdd7effbb8052e359e35e5d6dc
Catriel Escobar <125727012+Catriel-Escobar@users.noreply.github.com>
2024-11-21 18:20:59 -0300

    add new loan, custom validation

--- backend/src/main/java/com/financial/dto/request/loan/RequestLoanSimulationDTO.java
+++ backend/src/main/java/com/financial/dto/request/loan/RequestLoanSimulationDTO.java
@@ -1,7 +1,14 @@
 package com.financial.dto.request.loan;
 
+import com.financial.dto.customValidation.ValidLoanRate;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
-public record RequestLoanSimulationDTO(BigDecimal requestedAmount,  // Monto solicitado
-                                       int termMonths         // Plazo en meses
+public record RequestLoanSimulationDTO(
+        @NotNull
+        BigDecimal requestedAmount,
+        @NotNull
+        @ValidLoanRate
+        int termMonths
 ) {}
