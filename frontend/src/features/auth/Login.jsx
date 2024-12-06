e7e10ce985c07def0c982b8014fd55d3e1611a75
Catriel Escobar <125727012+Catriel-Escobar@users.noreply.github.com>
2024-12-06 12:41:29 -0300

    Fix logo

--- frontend/src/features/auth/Login.jsx
+++ frontend/src/features/auth/Login.jsx
@@ -1,8 +1,8 @@
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
 
-import FormRow from "../../ui/FormRow";
-import { useLogin } from "./useLogin";
-import SubmitButton from "../../ui/SubmitButton";
+import FormRow from '../../ui/FormRow';
+import { useLogin } from './useLogin';
+import SubmitButton from '../../ui/SubmitButton';
 
 const Login = ({ children }) => {
   const { login, isPending: isLoading } = useLogin();
@@ -19,70 +19,59 @@ const Login = ({ children }) => {
   }
 
   return (
-    <div className=" min-h-[90vh] flex flex-col m-auto ">
-      <h1 className=" text-3xl font-semibold mb-6 px-7">Iniciar sesión</h1>
+    <div className=' min-h-[90vh] flex flex-col m-auto '>
+      <h1 className=' text-3xl font-semibold mb-6 px-7'>Iniciar sesión</h1>
 
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-grow flex-col"
-      >
-        <div className="px-7  flex flex-col gap-5">
-          <FormRow label="Email" error={errors?.email?.message}>
+        className='flex flex-grow flex-col'>
+        <div className='px-7  flex flex-col gap-5'>
+          <FormRow label='Email' error={errors?.email?.message}>
             <input
-              type="email"
-              id="email"
-              placeholder="Email"
+              type='email'
+              id='email'
+              placeholder='Email'
               disabled={isLoading}
-              {...register("email", {
-                required: "Este campo es obligatorio",
+              {...register('email', {
+                required: 'Este campo es obligatorio',
                 pattern: {
                   value: /\S+@\S+\.\S+/,
                   message:
-                    "Proporcione una dirección de correo electrónico válida",
+                    'Proporcione una dirección de correo electrónico válida',
                 },
               })}
             />
           </FormRow>
 
-          <FormRow label="Contraseña" error={errors?.password?.message}>
+          <FormRow label='Contraseña' error={errors?.password?.message}>
             <input
-              type="password"
-              id="password"
-              placeholder="Ingresa tu contraseña"
-              {...register("password", {
-                required: "Este campo es obligatorio",
+              type='password'
+              id='password'
+              placeholder='Ingresa tu contraseña'
+              {...register('password', {
+                required: 'Este campo es obligatorio',
                 minLength: {
                   value: 8,
-                  message: "Contraseña",
+                  message: 'Contraseña',
                 },
                 pattern: {
                   value:
                     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).+$/,
                   message:
-                    "La contraseña debe contener al menos una letra mayúscula, una minúscula, un número y un carácter especial.",
+                    'La contraseña debe contener al menos una letra mayúscula, una minúscula, un número y un carácter especial.',
                 },
               })}
             />
           </FormRow>
 
-          <div className="flex justify-between">
-            <div className="flex gap-2 items-center">
-              <input
-                className=" w-4 h-4"
-                type="checkbox"
-                id="Recordarme"
-                name="Recordarme"
-              />
-              <label htmlFor="Recordarme">Recordarme</label>
-            </div>
-
-            <p className=" underline">Olvide mi contraseña</p>
+          <div className='flex flex-col items-center justify-between'>
+            <p className=' underline'>Olvide mi contraseña</p>
           </div>
         </div>
 
-        <div className="mt-[auto]">{children}</div>
+        <div className='mt-[auto]'>{children}</div>
 
-        <div className="border-t-[1px] border-lightGrey p-4">
+        <div className='border-t-[1px] border-lightGrey p-4'>
           <SubmitButton isPending={isLoading}>Iniciar sesión</SubmitButton>
         </div>
       </form>
--- frontend/src/ui/Nav.jsx
+++ frontend/src/ui/Nav.jsx
@@ -1,29 +1,33 @@
-import { AlignJustify } from "lucide-react";
-import { Link } from "react-router-dom";
+import { AlignJustify } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
-import Button from "./Button";
-import UserPhoto from "../features/user/UserPhoto";
-import useCurrentUser from "../features/user/useCurrentUser";
-import { Logo } from "../ui/Logo";
+import Button from './Button';
+import UserPhoto from '../features/user/UserPhoto';
+import useCurrentUser from '../features/user/useCurrentUser';
+
+import { LogoSvg } from './LogoSvg';
+import TextLogo from './TextLogo';
 
 const Nav = () => {
   const { user } = useCurrentUser();
 
   return (
-    <nav className="border-b-2 border-lightGrey p-4">
-      <ul className="flex items-center gap-2">
+    <nav className='border-b-2 border-lightGrey p-4'>
+      <ul className='flex items-center gap-2'>
         <li>
-          <AlignJustify className=" text-[dark]" />
+          <AlignJustify className=' text-[dark]' />
         </li>
 
-        <li className="px-4 py-1 font-medium">
-          <Link to="/home">
-            <Logo />
+        <li className=' text-xs font-medium flex items-center '>
+          <Link to='/home' className='flex items-center gap-1'>
+            {' '}
+            <LogoSvg height={28} width={28} />
+            <TextLogo height={12.84} width={76.77} />
           </Link>
         </li>
 
-        <li className="ml-[auto]  ">
-          {user ? <UserPhoto /> : <Button to="/auth">Iniciar sesión</Button>}
+        <li className='ml-[auto]  '>
+          {user ? <UserPhoto /> : <Button to='/auth'>Iniciar sesión</Button>}
         </li>
       </ul>
     </nav>
