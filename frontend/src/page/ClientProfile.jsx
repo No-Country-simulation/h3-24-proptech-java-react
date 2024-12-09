710b7fe982c9336f58d8cc138a88203c815510e6
Catriel Escobar <125727012+Catriel-Escobar@users.noreply.github.com>
2024-12-09 18:59:56 -0300

    fix menu

--- frontend/src/page/ClientProfile.jsx
+++ frontend/src/page/ClientProfile.jsx
@@ -1,12 +1,12 @@
-import ProfileMenu from "../features/user/ProfileMenu";
-import Button from "../ui/Button";
+import ProfileMenu from '../features/user/ProfileMenu';
+import Button from '../ui/Button';
 
 function ClientProfile() {
   return (
-    <section className=" min-h-[86vh] flex flex-col px-4 pt-5">
+    <section className=' min-h-[86vh] flex flex-col px-4 pt-5 pb-8'>
       <ProfileMenu />
 
-      <Button type="secondary" to="/loan-simulation" extraClass="mt-8">
+      <Button type='secondary' to='/loan-simulation' extraClass='mt-8'>
         Iniciar Solicitud
       </Button>
     </section>
