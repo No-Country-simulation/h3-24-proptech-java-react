75d4e753dcf3716828db8f43571d7e35a68d48c6
HikingCarrot7 <ricardoibarra2044@gmail.com>
2024-12-09 17:47:41 -0600

    Add integration with mercado pago in payment quotas page

--- frontend/src/features/loan/quotas/Quotas.jsx
+++ frontend/src/features/loan/quotas/Quotas.jsx
@@ -1,107 +1,179 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import { initMercadoPago, Wallet } from '@mercadopago/sdk-react';
+import axios from 'axios';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import Button from '../../../ui/Button';
+import { baseURL } from '../../../utils/constants';
+import { formatDate, formatToARSCurrency } from '../../../utils/helpers';
+import { getData } from '../../../utils/saveDataLocalStore';
+import useLoanDetails from '../useLoanDetails';
+import useGetPendingPaymentOfType from './useGetPendingPaymentOfType';
 
-import { formatNumber } from "../../../utils/helpers";
-import { useLoanSimulationResult } from "../useLoanSimulationResult";
+initMercadoPago('APP_USR-a5e2631a-7fd1-4654-8431-a36d05b602cb');
 
-function Quotas() {
+function Quotas({ loanId }) {
   const navigate = useNavigate();
-  const [show, setShow] = useState(false);
-  const loan = useLoanSimulationResult();
-  useEffect(
-    function () {
-      if (!loan) navigate("/messagesStartingLoan", { replace: true });
-    },
-    [loan, navigate]
-  );
+  const { loanDetails, isLoading } = useLoanDetails(loanId);
+  const loan = loanDetails?.loan;
 
-  const nextPayment = loan?.schedule?.find((payment) => payment.remaining > 0);
-  const remainingMonths = loan?.schedule.filter(
-    (payment) => payment.remaining > 0
-  );
-  const paidMonths = loan?.termMonths - remainingMonths?.length;
+  if (isLoading) {
+    return (
+      <div className="mt-[50px] flex justify-center font-bold text-3xl">
+        Cargando pagos...
+      </div>
+    );
+  }
+
+  if (!loan) {
+    return (
+      <div className="mt-[50px] flex justify-center font-bold text-3xl">
+        Préstamo no encontrado :(
+      </div>
+    );
+  }
+
+  if (loan.status !== 'APPROVED') {
+    return navigate('/loans');
+  }
 
   return (
-    <section className="max-w-[500px] mx-auto">
-      <h2 className=" text-3xl font-semibold mb-3">Tabla de cuotas</h2>
-      <p className="mb-7">Paga y administra tus cuotas pendientes.</p>
-
-      <div className="grid  grid-cols-2 gap-4 mb-7 items-center w-full">
-        <div className="bg-[#F1F5F9] text-center text-dark p-3  rounded-md flex items-center justify-center flex-col">
-          <p className=" text-sm font-medium text-dark">Total a pagar</p>
-          <p className=" text-lg font-bold min-[450px]:text-2xl">
-            {formatNumber(loan?.totalPayment)} USD
-          </p>
-        </div>
-
-        <div className="bg-[#F1F5F9] text-center text-dark p-3 rounded-md   flex items-center justify-center flex-col">
-          <p className=" text-sm font-medium text-dark">Cuotas</p>
-          <p className="text-lg font-bold min-[450px]:text-2xl">
-            {paidMonths}
-            <span className="font-normal">/</span>
-            <span className="text-base min-[450px]:text-xl  font-medium">
-              {loan?.termMonths}
-            </span>
-          </p>
+    <div className="mt-[50px] mx-10">
+      <div>
+        <div className="md:flex justify-between items-center">
+          <div>
+            <h3 className="mb-1 font-bold leading-none tracking-tight text-gray-900 text-2xl">
+              Cuotas a pagar
+            </h3>
+            <p className="mb-6 text-sm text-gray-500 dark:text-gray-400">
+              Paga y administra tus quotas pendientes
+            </p>
+          </div>
+          <div className="flex space-x-3">
+            <div className="rounded-lg bg-gray-100 p-3">
+              <h4 className="text-sm">Total a pagar</h4>
+              <p className="text-lg font-bold">
+                {formatToARSCurrency(loan.totalPayment)} ARS
+              </p>
+            </div>
+            <div className="rounded-lg bg-gray-100 p-3 min-w-[100px]">
+              <h4 className="text-sm">Cuotas</h4>
+              <p>
+                <span className="font-bold text-lg">{1}</span> /{' '}
+                <span className="text-sm">{loan.termMonths}</span>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-
-      {nextPayment && (
-        <div className="mb-7  flex justify-between items-center gap-2">
-          <div>
-            <small className="text-sm">Cuota {nextPayment.month}</small>
-            <p className=" text-xl font-semibold">
-              {formatNumber(nextPayment.quota)} USD
-            </p>
-            <small className="text-sm">
-              Saldo restante: {formatNumber(nextPayment.remaining)} USD
-            </small>
-          </div>
-
-          <button className="px-3 py-1 text-base text-light font-medium bg-primary rounded-lg ">
-            Pagar
-          </button>
-        </div>
-      )}
-
-      <h3
-        className="mb-7 text-lg font-medium flex items-center justify-between"
-        role="button"
-        onClick={() => setShow((prev) => !prev)}
-      >
-        Adelanta cuotas
-        {show ? <ChevronUp /> : <ChevronDown />}
-      </h3>
-
-      {show && (
-        <ul>
-          {remainingMonths.map((payment) => (
-            <li key={payment.month}>
-              <div className="mb-7 flex justify-between items-center gap-2">
-                <div>
-                  <small className="text-sm">Cuota {payment.month}</small>
-                  <p className=" text-xl font-semibold flex items-center gap-3">
-                    {formatNumber(payment.quota)} USD
-                    <span className=" px-2 py-1 border-[#22C55E] border-[1px] rounded-full  font-semibold  text-xs text-[#22C55E]">
-                      {payment.interest.toFixed(2)}%
-                    </span>
-                  </p>
-                  <small className="text-sm">
-                    Saldo restante: {formatNumber(payment.remaining)} USD
-                  </small>
-                </div>
-
-                <button className="px-3 py-1 text-base text-light font-medium bg-primary rounded-lg ">
-                  Pagar
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
-    </section>
+      <hr className="mt-3" />
+      <div className="w-full mt-5 space-y-10 xl:space-y-0 xl:flex">
+        <PendingPayment
+          loanId={loanId}
+          title="Cuota actual"
+          emptyPaymentTitle="No hay cuotas disponibles para pagar o ya han side pagadas las de este mes."
+          paymentType="ON_TIME"
+        />
+        <PendingPayment
+          loanId={loanId}
+          title="Adelanta cuotas"
+          emptyPaymentTitle="No hay coutas disponibles para adelantar o ya han sido adelantadas la de este mes."
+          paymentType="ADVANCE"
+        />
+      </div>
+    </div>
   );
 }
+
+const PendingPayment = ({ title, emptyPaymentTitle, loanId, paymentType }) => {
+  const [isCreatingPreference, setIsCreatingPreference] = useState(false);
+  const [preferenceId, setPreferenceId] = useState('');
+  const { pendingPayment, isLoading, isError } = useGetPendingPaymentOfType(
+    loanId,
+    paymentType
+  );
+
+  function createPreference(paymentId) {
+    const token = getData('token');
+    axios
+      .post(`${baseURL}/api/preferences/${paymentId}`, null, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setPreferenceId(response.data.preferenceId);
+      })
+      .catch(() => {
+        toast.error('Algo salió mal. Inténtelo de nuevo.');
+      })
+      .finally(() => {
+        setIsCreatingPreference(false);
+      });
+  }
+
+  if (isLoading) {
+    return <div>Cargando...</div>;
+  }
+
+  return (
+    <div className="flex-1 px-3">
+      <h4 className="text-md font-bold">{title}</h4>
+      <hr className="mt-2" />
+      {isError ? (
+        <div className="text-md text-center mt-3">{emptyPaymentTitle}</div>
+      ) : (
+        <div className="mt-5 sm:flex items-center justify-between">
+          <div className="space-y-3">
+            <div className="text-md">
+              Cuota {pendingPayment.installmentNumber}
+            </div>
+            <div className="flex items-center">
+              <div className="text-2xl font-bold">
+                {formatToARSCurrency(
+                  pendingPayment.discountAmount
+                    ? pendingPayment.totalAmountWithDiscount
+                    : pendingPayment.amount
+                )}{' '}
+                ARS
+              </div>
+              {pendingPayment.discountAmount ? (
+                <div className="rounded-lg border border-green-300 ml-3 px-2.5 text-center text-sm transition-all shadow-sm text-green-600">
+                  - {pendingPayment.discountAmount} %
+                </div>
+              ) : null}
+            </div>
+            <div className="font-light text-gray-400">
+              <span>{formatDate(pendingPayment.dueDate)}</span> -{' '}
+              <span>{formatDate(pendingPayment.payLimitDate)}</span>
+            </div>
+          </div>
+          <div className="mt-3 sm:mt-0">
+            {preferenceId ? (
+              <div id="wallet_container">
+                <Wallet
+                  initialization={{ preferenceId }}
+                  customization={{
+                    texts: { action: 'pay', valueProp: 'smart_option' },
+                  }}
+                />
+              </div>
+            ) : (
+              <Button
+                disabled={isCreatingPreference}
+                isLoading={isCreatingPreference}
+                type={paymentType === 'ADVANCE' ? 'primary' : 'secondary'}
+                extraClass={'min-w-[150px] w-full sm:w-auto'}
+                onClick={() => createPreference(pendingPayment.paymentId)}
+              >
+                Pagar
+              </Button>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Quotas;
--- frontend/src/features/user/ProfileMenu.jsx
+++ frontend/src/features/user/ProfileMenu.jsx
@@ -1,14 +1,14 @@
+import { useQueryClient } from '@tanstack/react-query';
 import { ChevronRight, LogOut } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
 import Button from '../../ui/Button';
-import { useQueryClient } from '@tanstack/react-query';
 
 const options = [
   { id: '1', label: 'Datos personales', to: '' },
   { id: '2', label: 'Panel de inversion', to: '/investmentPanel' },
   { id: '3', label: 'Datos de garantes', to: '' },
   { id: '4', label: 'Resumen de tu préstamo', to: '/loan/data-summary' },
-  { id: '5', label: 'Cuotas a pagar', to: '/paymentQuotas' },
+  { id: '5', label: 'Cuotas a pagar', to: '/loans' },
   { id: '6', label: 'Ajustes personales', to: '/personalSettings' },
 ];
 
@@ -18,20 +18,20 @@ function ProfileMenu() {
 
   function logout() {
     localStorage.removeItem('token');
-
     queryClient.invalidateQueries(['user']);
     queryClient.resetQueries(['user']);
-
-    navigate('/home');
+    return navigate('/home');
   }
+
   return (
     <div>
-      <ul className='flex-grow flex flex-col gap-5'>
+      <ul className="flex-grow flex flex-col gap-5">
         {options.map((option) => (
           <li key={option.id}>
             <Link
               to={option.to}
-              className='bg-[#F8FAFC] flex justify-between p-4 rounded-xl transition  hover:bg-primary hover:text-light'>
+              className="bg-[#F8FAFC] flex justify-between p-4 rounded-xl transition hover:bg-primary hover:text-light"
+            >
               {option.label}
               <ChevronRight />
             </Link>
@@ -39,11 +39,11 @@ function ProfileMenu() {
         ))}
       </ul>
 
-      <div className=' p-5'>
-        <h2 className=' text-lg font-bold mb-4'>Ajustes de sesión</h2>
-        <Button type='logOut' onClick={logout}>
-          <div className='flex gap-2 items-center'>
-            <LogOut className='w-[20px] h-[20px]' />
+      <div className=" p-5">
+        <h2 className=" text-lg font-bold mb-4">Ajustes de sesión</h2>
+        <Button type="logOut" onClick={logout}>
+          <div className="flex gap-2 items-center">
+            <LogOut className="w-[20px] h-[20px]" />
             Cerrar Sesión
           </div>
         </Button>
